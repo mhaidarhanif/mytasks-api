a3e0826ced05eb8b1c4f7851d18b4f711e69b1b5
M Haidar Hanif <git@mhaidarhanif.com>
2018-08-03 15:28:51 +0700

    Refactor to read & write JSON file via node fs

--- tasks/index.js
+++ tasks/index.js
@@ -1,37 +1,45 @@
-const DATA = {
-  counter: 3,
-  tasks: [
-    {
-      id: 1,
-      text: "Write first task"
-    },
-    {
-      id: 2,
-      text: "Schedule task action"
-    },
-    {
-      id: 3,
-      text: "Eat dinner"
-    }
-  ]
+const fs = require("fs")
+
+const DATA_JSON = "../data/index.json"
+
+const file = {
+  read: () => {
+    const DATA = JSON.parse(fs.readFileSync(DATA_JSON, "utf8"))
+    return DATA
+  },
+
+  write: NEW_DATA => {
+    const DATA_STRING = JSON.stringify(NEW_DATA, null, 2)
+    fs.writeFileSync(DATA_JSON, DATA_STRING, "utf8")
+  }
 }
 
 const tasks = {
   // ---------------------------------------------------------------------------
   // GET /tasks
   get: (req, res) => {
-    res.status(200).send(DATA.tasks)
+    const DATA = file.read()
+
+    if (DATA) res.status(200).send(DATA.tasks)
+    else
+      res.status(404).send({
+        action: `Get All`,
+        message: `Tasks not found`
+      })
   },
 
   // ---------------------------------------------------------------------------
   // GET /tasks/:id
   getOneById: (req, res) => {
+    const DATA = file.read()
     const task = DATA.tasks.find(item => {
       return item.id === Number(req.params.id)
     })
+
     if (task) res.status(200).send(task)
     else
       res.status(404).send({
+        action: `Get One`,
         message: `Task not found by id ${req.params.id}`
       })
   },
@@ -39,6 +47,7 @@ const tasks = {
   // ---------------------------------------------------------------------------
   // POST /tasks
   createOne: (req, res) => {
+    const DATA = file.read()
     DATA.counter += 1
 
     const newTask = {
@@ -47,10 +56,12 @@ const tasks = {
     }
 
     DATA.tasks.push(newTask)
+    file.write(DATA)
 
     if (newTask) res.status(201).send(newTask)
     else
       res.status(500).send({
+        action: `Create One`,
         message: `Error cannot create new task`
       })
   },
@@ -58,6 +69,7 @@ const tasks = {
   // ---------------------------------------------------------------------------
   // GET /tasks/search?text=first
   searchWithQuery: (req, res) => {
+    const DATA = file.read()
     const text = req.query.text
     const data = DATA.tasks.filter(item => {
       return item.text.toLowerCase().includes(text.toLowerCase())
@@ -74,6 +86,7 @@ const tasks = {
   // ---------------------------------------------------------------------------
   // POST /tasks/search
   searchWithBody: (req, res) => {
+    const DATA = file.read()
     const text = req.body.text
     const data = DATA.tasks.filter(item => {
       return item.text.toLowerCase().includes(text.toLowerCase())
