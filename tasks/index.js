cd36d75605400dca2fb35b5e1bc24d1a91f190c3
M Haidar Hanif <git@mhaidarhanif.com>
2018-08-03 14:57:57 +0700

    Add more seed tasks and implement search method
    Using built-in filter and match for lowercased text

--- tasks/index.js
+++ tasks/index.js
@@ -8,6 +8,10 @@ const DATA = {
     {
       id: 2,
       text: "Schedule task action"
+    },
+    {
+      id: 3,
+      text: "Eat dinner"
     }
   ]
 }
@@ -26,7 +30,7 @@ const tasks = {
     if (task) res.status(200).send(task)
     else
       res.status(404).send({
-        message: `Task not found`
+        message: `Task not found by id ${req.params.id}`
       })
   },
 
@@ -45,6 +49,20 @@ const tasks = {
     else
       res.status(500).send({
         message: `Error cannot create new task`
+      })
+  },
+
+  // GET /tasks/search?text=first
+  search: (req, res) => {
+    const text = req.query.text
+    const data = DATA.tasks.filter(item => {
+      return item.text.toLowerCase().includes(text.toLowerCase())
+    })
+
+    if (data) res.status(200).send(data)
+    else
+      res.status(404).send({
+        message: `Search task is not found`
       })
   }
 }
